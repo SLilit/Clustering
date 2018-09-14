22f463d15c160655566d6cb256396fd7c9aaa9ea
LILIT <lilit555ai@gmail.com>
2018-09-13 21:55:40 -0600

    Add data reading

--- hw3_clustering.py
+++ hw3_clustering.py
@@ -5,7 +5,6 @@ import sys
 import math
 from random import randint
 
-
-
-    
-
+X = np.genfromtxt(sys.argv[1], delimiter = ",") 
+centers = [randint(0,len(X)) for i in range(5)]
+centerslist = np.array([X[i] for i in centers])
